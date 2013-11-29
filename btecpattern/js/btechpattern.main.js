afc9f239287bd21d7cf39d5175292f2a63831306
Mike Speight <speight.mike@gmail.com>
2013-11-29 15:34:47 +0000

    completed signposting functionality

--- btecpattern/js/btechpattern.main.js
+++ btecpattern/js/btechpattern.main.js
@@ -7,35 +7,36 @@
 
     var acddStr = '<div class="signposting-dropdown acceptance-criteria"><ul>';
     acddStr += '<li><p class="title">Functional Skills Signposting</p></li>';
-    acddStr += '<li data-criteria = "english"><p class="cat-title">English</p></li>';
-    acddStr += '<li data-criteria = "english"><label><input type="checkbox" /> <span>Speaking, listening and communication</span></label></li>';
-    acddStr += '<li data-criteria = "english"><label><input type="checkbox" /> <span>Reading</span></label></li>';
-    acddStr += '<li data-criteria = "english"><label><input type="checkbox" /> <span>Writing</span></label></li>';
-    acddStr += '<li data-criteria = "mathematics"><p class="cat-title">Mathematics</p></li>';
-    acddStr += '<li data-criteria = "mathematics"><label><input type="checkbox" /> <span>Representing</span></label></li>';
-    acddStr += '<li data-criteria = "mathematics"><label><input type="checkbox" /> <span>Analysing</span></label></li>';
-    acddStr += '<li data-criteria = "mathematics"><label><input type="checkbox" /> <span>Interpreting</span></label></li>';
-    acddStr += '<li data-criteria = "ict"><p class="cat-title">ICT</p></li>';
-    acddStr += '<li data-criteria = "ict"><label><input type="checkbox"/> <span>Using ICT</span></label></li>';
-    acddStr += '<li data-criteria = "ict"><label><input type="checkbox"/> <span>Finding and selecting information</span></label></li>';
-    acddStr += '<li data-criteria = "ict"><label><input type="checkbox"/> <span>Developing, presenting and communicating information Using ICT</span></label></li>';
+    acddStr += '<li><p class="cat-title">English</p></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Speaking, listening and communication</span></label></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Reading</span></label></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Writing</span></label></li>';
+    acddStr += '<li><p class="cat-title">Mathematics</p></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Representing</span></label></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Analysing</span></label></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Interpreting</span></label></li>';
+    acddStr += '<li><p class="cat-title">ICT</p></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Using ICT</span></label></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Finding and selecting information</span></label></li>';
+    acddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Developing, presenting and communicating information Using ICT</span></label></li>';
     acddStr += '</ul></div>';
     
     var pltsddStr = '<div class="signposting-dropdown plts-criteria"><ul>';
     pltsddStr += '<li><p class="title">Personal Learning and Thinking Skils (PLTS) Signposting</p></li>';
-    pltsddStr += '<li data-criteria = "plst"><label><input type="checkbox" /> <span>Independent Enquirers</span></label></li>';
-    pltsddStr += '<li data-criteria = "plst"><label><input type="checkbox" /> <span>Creative Thinkers</span></label></li>';
-    pltsddStr += '<li data-criteria = "plst"><label><input type="checkbox" /> <span>Reflective Learners</span></label></li>';
-    pltsddStr += '<li data-criteria = "plst"><label><input type="checkbox" /> <span>Team Workers</span></label></li>';
-    pltsddStr += '<li data-criteria = "plst"><label><input type="checkbox" /> <span>Self Managers</span></label></li>';
-    pltsddStr += '<li data-criteria = "plst"><label><input type="checkbox" /> <span>Effective Participants</span></label></li>';
+    pltsddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Independent Enquirers</span></label></li>';
+    pltsddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Creative Thinkers</span></label></li>';
+    pltsddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Reflective Learners</span></label></li>';
+    pltsddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Team Workers</span></label></li>';
+    pltsddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Self Managers</span></label></li>';
+    pltsddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Effective Participants</span></label></li>';
     pltsddStr += '</ul></div>';
     
     var scddStr = '<div class="signposting-dropdown subject-criteria"><ul>';
     scddStr += '<li><p class="title">GCSE Subject Criteria Signposting</p></li>';
-    scddStr += '<li><label><input data-criteria = "English" type="checkbox"/> <span>English</span></label></li>';
-    scddStr += '<li><label><input data-criteria="Mathmatics" type="checkbox"/> <span>Mathmatics</span></label></li>';
+    scddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>English</span></label></li>';
+    scddStr += '<li><label><input data-criteria = "" type="checkbox" /><span>Mathmatics</span></label></li>';
     scddStr += '</ul></div>';
+
 
     $(document).ready(function(){
 
@@ -63,15 +64,61 @@
             $('.signposting-dropdown').hide(500);
          };
 
+         var updateCriteria = function($this, $criteriaContainer){
+
+            var $checkedCheckbox = $this.find('input:checked');
+
+            if($checkedCheckbox.length > 0){
+                //console.log('Checked');
+
+                var buttonStr = '<button class="badge" data-criteria="';
+                    buttonStr += $checkedCheckbox.data('criteria');
+                    buttonStr += '">';
+                    buttonStr += $checkedCheckbox.data('criteria');
+                    buttonStr += '</button>';
+
+                $criteriaContainer.append(buttonStr);
+            }
+            else {
+                //console.log('Unchecked'); 
+                //loop through buttons in container and if button data attribute equals input data attribute remove it
+                var $buttons = $criteriaContainer.find('button'),
+                    $checkboxData = $this.find('input').data('criteria');
+
+                    for (var i = $buttons.length - 1; i >= 0; i--) {
+                        //console.log('looping',$buttons.length, 'times')
+
+                        var $thisButton = $buttons[i],
+                            buttonData = $($buttons[i]).data('criteria');
+
+                        console.log(buttonData);
+                        console.log($checkboxData);
+
+                        if(buttonData === $checkboxData){
+                            $thisButton.remove();
+                        }
+                    };
+
+            }
+
+         };
+
          var initDropdown = function($dropdown, $criteriaContainer){
 
             var $collection = $criteriaContainer.find('button'),
-                $checkboxes = $dropdown.find("li label input[type='checkbox']");
+                $checkboxes = $dropdown.find("li label input[type='checkbox']"),
+                $checkboxLabel = $dropdown.find('li label');
 
-                console.log('checkboxes', $checkboxes.length);
+
+            //set data atteributes for checkboxes
+            for (var count = $checkboxes.length - 1; count >= 0; count--) {
+                
+                var $thisLabel = $($checkboxLabel[count]).text();
+
+                $($checkboxes[count]).data('criteria', $thisLabel);
+            };
 
             if($collection.length > 0){
-                console.log('the collection contains ',$collection.length ,' items.');
 
                 var collectionItems = [];
 
@@ -85,13 +132,15 @@
                         var $criteria = $($checkboxes[x]).data('criteria');
 
                         if(collectionItems[i] === $criteria){
-                            //console.log('true');
+                            console.log('true');
                             // set checkbox
                             $($checkboxes[x]).attr('checked','checked');
                         }
+                        else {
+                            console.log('false')
+                        }
                     };
                 };
-                //console.log('collection = ',collectionItems);
 
                 return;
             }
@@ -102,28 +151,64 @@
          };
 
 
+         $('body').on('change', '.signposting-dropdown li input', function(e){
+
+            console.log('changed checkbox')
+
+            var $this = $(this).parents('li'),
+                $dropdown = $this.parents('.signposting-dropdown'),
+                $criteriaContainer;
+
+            if($dropdown.hasClass('acceptance-criteria')) {
+                // assessment Criteria
+                $criteriaContainer = $dropdown.parents('.ass-criteria-row'); 
+                updateCriteria($this, $criteriaContainer);
+                e.stopPropagation();
+            }
+            else if($dropdown.hasClass('plts-criteria')) {
+                // PLTS Criteria
+                $criteriaContainer = $dropdown.parents('.plts-criteria-row'); 
+                updateCriteria($this, $criteriaContainer);
+                e.stopPropagation();
+
+            }
+            else if($dropdown.hasClass('subject-criteria')) {
+                // Subject Criteria
+                $criteriaContainer = $dropdown.parents('.categories'); 
+                updateCriteria($this, $criteriaContainer);
+                e.stopPropagation();
+            }
+            else{
+
+                return;
+            }
+
+
+         });
 
     
          $('body').on("click", ".dropdownToggle", function(e){
 
             var $this = $(this),
-                $dropdown;
+                $dropdown,
+                $criteriaContainer;
 
             if($this.hasClass('acddToggle')){
                 
                 $dropdown = $this.parents('.panel-heading').siblings('.panel-collapse').find(".ass-criteria-row .signposting-dropdown");
-                $criteriaContainer = $dropdown.parents('.ass-criteria-row');
+                $criteriaContainer = $dropdown.parents('.ass-criteria-row');                
+                initDropdown($dropdown, $criteriaContainer);
             }
             else if($this.hasClass('pltsddToggle')){
                 
                 $dropdown = $this.parents('.panel-heading').siblings('.panel-collapse').find(".plts-criteria-row .signposting-dropdown");
                 $criteriaContainer = $dropdown.parents('.plts-criteria-row');
+                initDropdown($dropdown, $criteriaContainer);
             }
             else if($this.hasClass('scddToggle')){
                 
                 $dropdown = $this.siblings(".signposting-dropdown");
-                $criteriaContainer = $criteriaContainer = $dropdown.parents('.categories');
-
+                $criteriaContainer = $dropdown.parents('.categories');
                 initDropdown($dropdown, $criteriaContainer);
 
             }
